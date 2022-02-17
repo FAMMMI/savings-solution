00a3cafccb91a6b49bc6b028885525251c14d530
FAMMMI <naimfami@gmail.com>
2022-02-17 20:11:14 +0600

    final

--- js/app.js
+++ js/app.js
@@ -10,6 +10,64 @@ const saveAmount = document.getElementById("savingAmount");
 const saveInput = document.getElementById("savingPercent");
 const remainingBalance = document.getElementById("remainingBalance");
 
+// clears the error message
+function clearError() {
+    const commentContainer1 = document.getElementById("error-div-1");
+    commentContainer1.innerText = "";
+    const commentContainer2 = document.getElementById("error-div-1");
+    commentContainer2.innerText = "";
+    const commentContainer3 = document.getElementById("error-div-2");
+    commentContainer3.innerText = "";
+    const commentContainer4 = document.getElementById("error-div-2");
+    commentContainer4.innerText = "";
+
+
+
+}
+
+// if gets an error message the clears the Expense, balance etc
+function clearInp() {
+    totalExpense.innerText = "";
+    balanceShow.innerText = "";
+    saveAmount.innerText = "";
+    saveInput.innerText = "";
+    remainingBalance.innerText = "";
+
+
+
+}
+
+// handles error messages
+function errorMessage(path) {
+    clearInp();
+    if (path == 1) {
+        const newComment = document.createElement("p");
+        newComment.innerText = "Please input a valid number.";
+
+        const commentContainer = document.getElementById("error-div-1");
+        commentContainer.appendChild(newComment);
+    } else if (path == 2) {
+        const newComment = document.createElement("p");
+        newComment.innerText = "Sorry, save amount exceeded balance.";
+
+        const commentContainer = document.getElementById("error-div-2");
+        commentContainer.appendChild(newComment);
+    } else if (path == 3) {
+        const newComment = document.createElement("p");
+        newComment.innerText = "Sorry, Expense can not exceed income.";
+
+        const commentContainer = document.getElementById("error-div-1");
+        commentContainer.appendChild(newComment);
+    } else if (path == 4) {
+        const newComment = document.createElement("p");
+        newComment.innerText = "Please input a valid number.";
+
+        const commentContainer = document.getElementById("error-div-2");
+        commentContainer.appendChild(newComment);
+    }
+
+}
+
 // gets the total sum
 function getSum(Rent, Clothes, Food) {
     let sum = Rent + Clothes + Food;
@@ -35,8 +93,9 @@ function checkNum() {
 
 // Starts working if the below button if clicked 
 document.getElementById("calculation").addEventListener("click", function () {
+    clearError();
     if (checkNum()) {
-
+        errorMessage(1);
     } else {
         const totalSum = getSum(
             parseFloat(foodInput.value),
@@ -48,14 +107,15 @@ document.getElementById("calculation").addEventListener("click", function () {
             const balanceInp =
                 parseFloat(incomeInput.value) - parseFloat(totalExpense.innerText);
             balanceShow.innerText = balanceInp;
-        }
+        } else errorMessage(3);
     }
 });
 
 // Starts working if the below button if clicked
 document.getElementById("savingButton").addEventListener("click", function () {
-
+    clearError();
     if (checkNum() || totalExpense.innerText == '' || balanceShow.innerText == '') {
+        errorMessage(4);
 
     } else {
         const saveBalance =
@@ -74,6 +134,7 @@ document.getElementById("savingButton").addEventListener("click", function () {
             const rem_balance = document.getElementById("remainingBalance");
             rem_balance.innerText = totalExpense;
         } else {
+            errorMessage(2);
 
         }
     }
